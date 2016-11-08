117bad9996c6aa32a63b3d2031e05297c1aaa6ba
donghao <dhao729176112@163.com>
2016-11-08 22:05:33 +0800

    Merge branch 'master' of https://github.com/Aicibg/DHFrame 111111

--- app/src/main/java/com/dhao/dhframe/MainActivity.java
+++ app/src/main/java/com/dhao/dhframe/MainActivity.java
@@ -1,15 +1,17 @@
 package com.dhao.dhframe;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
 import com.dhao.framelibrary.base.BaseAppCompatActivity;
+import com.orhanobut.logger.Logger;
 
 public class MainActivity extends BaseAppCompatActivity {
     private Button btStart,btActivity;
     private FirstFragment firstFragment;
+    private String str="abs";
 
     @Override
     protected int getContentViewLayoutID() {
@@ -18,12 +20,16 @@ public class MainActivity extends BaseAppCompatActivity {
 
     @Override
     protected void initViews(Bundle savedInstanceState) {
+        Logger.e("str="+str);
         btStart= (Button) findViewById(R.id.button);
         btActivity= (Button) findViewById(R.id.bt_start_activity);
 
         btStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Logger.e("str="+str);
+//                int a=Integer.valueOf(str);
+
                 firstFragment.start(new BackFragment());
                 showSnack("启动Fragment", btStart, Color.parseColor("#239120"), new View.OnClickListener() {
                     @Override
--- app/src/main/java/com/dhao/dhframe/MyApplication.java
+++ app/src/main/java/com/dhao/dhframe/MyApplication.java
@@ -1,9 +1,30 @@
 package com.dhao.dhframe;
 
+import android.app.Application;
+
+import com.orhanobut.logger.LogLevel;
+import com.orhanobut.logger.Logger;
+import com.tencent.bugly.crashreport.CrashReport;
+
 /**
  * Created by DongHao on 2016/11/7
  * Description:
  */
 
-public class MyApplication {
+public class MyApplication extends Application {
+
+    private String YOUR_TAG="MyTest";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        CrashReport.initCrashReport(getApplicationContext(), "e148aa2f3e", true);
+
+        Logger.init(YOUR_TAG)                 // default PRETTYLOGGER or use just init()
+                .methodCount(1)                 // default 2
+                .hideThreadInfo()               // default shown
+                .logLevel(LogLevel.FULL)        // default LogLevel.FULL
+                .methodOffset(2)                // default 0
+                .logAdapter(new MyLogAdapter()); //default AndroidLogAdapter
+    }
 }
